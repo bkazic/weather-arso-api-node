66f3ed4d08033e64e303f7f50eeab2b0718045c0
bkazic <blaz.kazic@ijs.si>
2015-02-25 16:26:23 +0100

    Simple arso weather request test

--- ArsoWeather/index.js
+++ ArsoWeather/index.js
@@ -1,10 +1,40 @@
 ﻿var request = require("request");
 
+// Default parameters when making request
+var defaultParams = {
+    lang: 'en',
+    vars: [12, 19, 13, 20, 14, 26, 2, 21, 15, 23, 16, 24, 17, 27, 4, 28, 18],
+    group: 'halfhourlyData0',
+    type: 'halfhourly',
+    id: 1828 // Ljubljana - Bezigrad
+};
 
-var defaultRequestOptions = {};
-
+// Main object
 function WeatherArso() {
     this.baseUrl = "http://meteo.arso.gov.si/webmet/archive/data.xml";
 }
 
+// Make weather request for specific time
+WeatherArso.prototype.weather = function (time, callback) {   
+    var url = this.buildUrl(time);
 
+    request(url, function (error, response, body) {
+        if (error) return callback(error);
+        return callback (null, body)
+    })
+}
+
+// TODO: Find better way to construct url (something like string format in python)
+// Build url request for specific time
+WeatherArso.prototype.buildUrl = function (time) {
+    var url = this.baseUrl + "?lang=" + defaultParams.lang + "&vars=" + 
+        defaultParams.vars.toString() + "&group=" + defaultParams.group + 
+        "&type=" + defaultParams.type + "&id=" + defaultParams.id + 
+        "&d1=" + time + "&d2=" + time
+
+    return url
+}
+
+// Exports
+module.exports = WeatherArso;
+    
--- ArsoWeather/test/test.js
+++ ArsoWeather/test/test.js
@@ -1,2 +1,14 @@
 ﻿var assert = require('assert');
+var WeatherArso = require('../');
 // TODO: Maybe do unit test with mocha
+
+var weatherArso = new WeatherArso()
+
+// Print constructed url
+console.log(weatherArso.buildUrl("2015-02-03"))
+
+// Request url and display output
+weatherArso.weather("2015-02-03", function (err, data) {
+    if (err) throw err;
+    console.log(data);
+})
