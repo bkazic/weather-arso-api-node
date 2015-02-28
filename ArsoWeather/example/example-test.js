851fc4ab886b8850edfb2124f20ae9b06fdceb82
bkazic <blaz.kazic@ijs.si>
2015-02-28 16:58:06 +0100

    Added examples folder. Added mocha unit test.

---
+++ ArsoWeather/example/example-test.js
@@ -0,0 +1,48 @@
+﻿var assert = require('assert');
+var WeatherArso = require('../');
+// TODO: Maybe do unit test with mocha
+
+var weatherArso = new WeatherArso()
+//var weatherArso = new WeatherArso({id: 1872})
+
+
+console.log(JSON.stringify(weatherArso.params))
+/*
+// Request url and display output
+weatherArso.weatherData("2015-02-03", function (err, data) {
+    if (err) throw err;
+    console.log(JSON.stringify(data));
+})
+
+// Print constructed url
+console.log(weatherArso.buildUrl("2015-02-03"))
+
+
+// Test request with start day and end day
+weatherArso.weatherData("2015-02-03", "2015-02-04", function (err, data) {
+    if (err) throw err;
+    console.log(JSON.stringify(data));
+})
+
+*/
+// Testing with options
+var options = {
+    vars: [12, 19, 13, 20, 14, 26, 2, 21, 15, 23, 16, 24, 17, 27, 4, 28, 18],
+    id: 2293
+}
+
+weatherArso.weatherData("2015-02-03", options, function (err, data) {
+    if (err) throw err;
+    console.log(JSON.stringify(data));
+})
+
+console.log(JSON.stringify(weatherArso.params))
+
+
+// Request url and display output
+weatherArso.weatherData("2015-02-03", function (err, data) {
+    if (err) throw err;
+    console.log(JSON.stringify(data));
+})
+
+console.log(JSON.stringify(weatherArso.params))
--- ArsoWeather/test/test.js
+++ ArsoWeather/test/test.js
@@ -5,43 +5,17 @@ var WeatherArso = require('../');
 var weatherArso = new WeatherArso()
 //var weatherArso = new WeatherArso({id: 1872})
 
-console.log(JSON.stringify(weatherArso.params))
-/*
-// Request url and display output
-weatherArso.weatherData("2015-02-03", function (err, data) {
-    if (err) throw err;
-    console.log(JSON.stringify(data));
-})
-
-// Print constructed url
-console.log(weatherArso.buildUrl("2015-02-03"))
-
-
-// Test request with start day and end day
-weatherArso.weatherData("2015-02-03", "2015-02-04", function (err, data) {
-    if (err) throw err;
-    console.log(JSON.stringify(data));
-})
-
-*/
-// Testing with options
-var options = {
-    vars: [12, 19, 13, 20, 14, 26, 2, 21, 15, 23, 16, 24, 17, 27, 4, 28, 18],
-    id: 2293
-}
-
-weatherArso.weatherData("2015-02-03", options, function (err, data) {
-    if (err) throw err;
-    console.log(JSON.stringify(data));
-})
-
-console.log(JSON.stringify(weatherArso.params))
-
-
-// Request url and display output
-weatherArso.weatherData("2015-02-03", function (err, data) {
-    if (err) throw err;
-    console.log(JSON.stringify(data));
-})
-
-console.log(JSON.stringify(weatherArso.params))
+describe('WeatherArso', function () {
+    describe('#weatherData()', function () {
+        it('should return weather data for Ljubljana Bezigrad for specific date', function (done) {
+            weatherArso.weatherData("2015-02-03", function (err, data) {
+                if (err) throw err;
+                
+                assert.ok(data);
+                assert.ok(data.id);
+                assert.ok(data.data)
+                done();
+            });
+        });
+    });
+});
