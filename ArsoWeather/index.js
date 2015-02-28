2140e1f2e70e4a8673233f02e6bf2b07b0c15c1e
bkazic <blaz.kazic@ijs.si>
2015-02-28 12:11:53 +0100

    Added checkOptions

--- ArsoWeather/index.js
+++ ArsoWeather/index.js
@@ -11,33 +11,38 @@ var defaultParams = {
 };
 
 // Main object
-function WeatherArso() {
+function WeatherArso(options) {
     this.baseUrl = "http://meteo.arso.gov.si/webmet/archive/data.xml";
-
-    this.lang = 'en';
-    this.vars = [12, 19],
-    this.group = 'halfhourlyData0',
-    this.type = 'halfhourly',
-    this.id = 1828 // Ljubljana - Bezigrad
+    if (typeof options !== "undefined") this.checkOptions(options);
+    this.params = defaultParams;
 }
 
 // Get weather data for specific time
 WeatherArso.prototype.weatherData = function (time, time2, options, callback) {
     var d1 = time;
     var d2 = time2;
-
+    
     if (typeof time2 === "function") {
         callback = time2;
         d2 = time;
         options = {};
     }
     
+    if (typeof time2 === "object") {
+        callback = options;
+        d2 = time;
+        options = time2;
+    }
+    
     if (typeof options === "function") {
         callback = options;
         options = {};
     }
+    
+    //TODO: Check Options!! 
+    this.checkOptions(options, true);
 
-    var url = this.buildUrl(d1, d2, options);
+    var url = this.buildUrl(d1, d2);
     this.makeRequest(url, function (err, data) {
         if (err) return callback(err);
         return callback(null, data);
@@ -46,15 +51,30 @@ WeatherArso.prototype.weatherData = function (time, time2, options, callback) {
 
 // TODO: Find better way to construct url (something like string format in python)
 // Build url request for specific time
-WeatherArso.prototype.buildUrl = function (d1, d2, options) {
-
-    var url = this.baseUrl + "?lang=" + defaultParams.lang + "&vars=" + 
-        defaultParams.vars.toString() + "&group=" + defaultParams.group + 
-        "&type=" + defaultParams.type + "&id=" + defaultParams.id + 
+WeatherArso.prototype.buildUrl = function (d1, d2) {
+    var url = this.baseUrl + "?lang=" + this.params.lang + "&vars=" + 
+        this.params.vars.toString() + "&group=" + this.params.group + 
+        "&type=" + this.params.type + "&id=" + this.params.id + 
         "&d1=" + d1 + "&d2=" + d2
 
     return url
 }
+
+
+// Check if any of options key are the same as in defaultParams. If yes, overwrite their value.
+WeatherArso.prototype.checkOptions = function (options, copy) {
+    // If copy is false, newParams will copy reference from defaultParams and make permanent change.
+    var newParams = (copy === true) ? JSON.parse(JSON.stringify(defaultParams)) : defaultParams; 
+
+    for (key in defaultParams) {
+        if (options[key]) {
+            newParams[key] = options[key];
+        }
+    }
+
+    this.params = newParams;
+}
+
 
 // Make request and parse the result
 WeatherArso.prototype.makeRequest = function (url, callback) {
@@ -62,7 +82,6 @@ WeatherArso.prototype.makeRequest = function (url, callback) {
         if (error) return callback(error);
         if (response.statusCode !== 200) throw new WeatherArsoError("Request Failed")
         
-        //TODO: parse body here!
         data = parseToJson(body)
      
         return callback(null, data)
@@ -85,6 +104,7 @@ function parseToJson(data) {
 function formatOutput(jsonData) {
     var obj = jsonData["points"]["_"+defaultParams.id];
     var outObj = {}
+
     outObj["data"] = []
 
     for (var key in obj) {
--- ArsoWeather/test/test.js
+++ ArsoWeather/test/test.js
@@ -2,8 +2,10 @@
 var WeatherArso = require('../');
 // TODO: Maybe do unit test with mocha
 
-var weatherArso = new WeatherArso()
+var weatherArso = new WeatherArso({id: 1872})
 
+console.log(JSON.stringify(weatherArso.params))
+/*
 // Request url and display output
 weatherArso.weatherData("2015-02-03", function (err, data) {
     if (err) throw err;
@@ -19,3 +21,24 @@ weatherArso.weatherData("2015-02-03", "2015-02-04", function (err, data) {
     if (err) throw err;
     console.log(JSON.stringify(data));
 })
+
+*/
+// Testing with options
+var options = {
+    vars: [12, 19, 13, 20, 14, 26, 2, 21, 15, 23, 16, 24, 17, 27, 4, 28, 18],
+}
+
+weatherArso.weatherData("2015-02-03", options, function (err, data) {
+    if (err) throw err;
+    //console.log(JSON.stringify(data));
+})
+
+console.log(JSON.stringify(weatherArso.params))
+
+// Request url and display output
+weatherArso.weatherData("2015-02-03", function (err, data) {
+    if (err) throw err;
+    //console.log(JSON.stringify(data));
+})
+
+console.log(JSON.stringify(weatherArso.params))
